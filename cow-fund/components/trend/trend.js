7f25ac30f960c238597f3f8eb2da3219f3ba2c9a
liulu <kjsx-liulu@cmbc.com.cn>
2019-05-28 09:59:47 +0800

    修改接口

--- cow-fund/components/trend/trend.js
+++ cow-fund/components/trend/trend.js
@@ -24,80 +24,78 @@ Component({
   didUpdate(prevProps, prevData) {
     const fundList = this.props.fundList;
     if (prevProps.fundList === null) {
-      const res = fundList.map(item => {
-        let simTrendList, realTrendList;
+      fundList.forEach(item => {
         my.request({
           url: `https://www.mdollar.cn/huaxia-alipay/index/v1/trendList?fundCode=${item.fundCode}`,
           method: 'GET',
           dataType: 'json',
           success: (res) => {
-            simTrendList = res.simTrendList.reduce((total, cur) => {
-              return total.concat(cur);
-            });
-            realTrendList = res.realTrendList.reduce((total, cur) => {
-              return total.concat(cur);
-            });
+            let simTrendList, realTrendList;
+            simTrendList = res.data.body.simTrendList;
+            realTrendList = res.data.body.realTrendList;
+
+            let date = [], simTrendValue = [], realTrendValue = [];
+            simTrendList.forEach(item => {
+              date.push(item.day);
+              simTrendValue.push(item.point);
+            })
+            realTrendList.forEach(item => {
+              realTrendValue.push(item.point);
+            })
+
+            // 保留近三年的数据
+            const { simDataList, realDataList } = this.data;
+            simDataList.push(simTrendValue);
+            realDataList.push(realTrendValue);
+            this.setData({
+              dateList: date,
+              simDataList: simDataList,
+              realDataList: realDataList
+            })
+            const result = Object.assign({}, item, {
+              categories: date.slice(0, 180),
+              series: [
+                {
+                  type: '华夏小牛模拟收益',
+                  color: '#FE4452',
+                  // style: 'smooth',
+                  data: simTrendValue.slice(0, 180)
+                },
+                {
+                  type: item.fundName,
+                  color: '#B5B5B5',
+                  // style: 'smooth',
+                  data: realTrendValue.slice(0, 180)
+                }
+              ]
+            })
+
+            const fundList_data = this.data.fundList;
+            fundList_data.push(result);
+            this.setData({
+              fundList: fundList_data
+            })
           }
         })
-        let date = [], simTrendValue = [], realTrendValue = [];
-        simTrendList.forEach(item => {
-          date.push(item.day);
-          simTrendValue.push(item.point);
-        })
-        realTrendList.forEach(item => {
-          realTrendValue.push(item.point);
-        })
-
-        // 保留近三年的数据
-        const {simDataList,realDataList} = this.data;
-        simDataList.push(simTrendValue);
-        realDataList.push(realTrendValue);
-        this.setData({
-          dateList: date,
-          simDataList: simDataList,
-          realDataList: realDataList
-        })
-
-        return Object.assign({}, item, {
-          categories: date.slice(0,180),
-          series: [
-            {
-              type: '华夏小牛模拟收益',
-              color: '#FE4452',
-              style: 'smooth',
-              data: simTrendValue.slice(0,180)
-            },
-            {
-              type: item.fundName,
-              color: '#B5B5B5',
-              style: 'smooth',
-              data: realTrendValue.slice(0,180)
-            },
-          ]
-        })
       })
-      this.setData({
-        fundList: res
-      })
+
     }
   },
   methods: {
     changeActive(e) {
       const active = Number(e.target.dataset.index);
-      const {index, fundList, dateList, simDataList, realDataList} = this.data;
-      if(active === 0){
-        const fund = fundList[`${index}`];
-        fund.categories = date.slice(0,180);
-        fund.series[0].data = simDataList[`${index}`].slice(0,180);
-        fund.series[1].data = realDataList[`${index}`].slice(0,180);
-      }else if(active === 1){
-        const fund = fundList[`${index}`];
-        fund.categories = date.slice(0,365);
-        fund.series[0].data = simDataList[`${index}`].slice(0,365);
-        fund.series[1].data = realDataList[`${index}`].slice(0,365);
-      }else{
-        const fund = fundList[`${index}`];
-        fund.categories = date.slice(0,180);
+      const { index, fundList, dateList, simDataList, realDataList } = this.data;
+      const fund = fundList[`${index}`];
+      if (active === 0) {
+        fund.categories = dateList.slice(0, 180);
+        fund.series[0].data = simDataList[`${index}`].slice(0, 180);
+        fund.series[1].data = realDataList[`${index}`].slice(0, 180);
+      } else if (active === 1) {
+        fund.categories = dateList.slice(0, 365);
+        fund.series[0].data = simDataList[`${index}`].slice(0, 365);
+        fund.series[1].data = realDataList[`${index}`].slice(0, 365);
+      } else {
+        fund.categories = dateList;
         fund.series[0].data = simDataList[`${index}`];
         fund.series[1].data = realDataList[`${index}`];
       }
